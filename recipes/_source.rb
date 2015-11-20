facbfcd7a9d40827d4d7c6abb90ad5a8322ea466
John Dyer <jdyer@tropo.com>
2015-11-20 15:16:12 -0500

    Add correct deps and artifact

--- recipes/_source.rb
+++ recipes/_source.rb
@@ -1,13 +1,16 @@
-%w(bzip2 asciidoc libxslt-devel docbook-style-xsl).each do |p|
+%w(bzip2 automake asciidoc libxslt-devel docbook-style-xsl).each do |p|
   package p
 end
 
 node.set['build_essential']['compile_time'] = true
 include_recipe "build-essential"
 
+
 ark "tinyproxy" do
-  url "https://files.banu.com/tinyproxy/1.8/tinyproxy-1.8.3.tar.bz2"
+  url "https://github.com/banu/tinyproxy/archive/1.8.3.tar.gz"
+  # https://files.banu.com/tinyproxy/1.8/tinyproxy-1.8.3.tar.bz2"
   version '1.8.3'
-  checksum 'be559b54eb4772a703ad35239d1cb59d32f7cf8a739966742622d57df88b896e'
+  checksum '24270804a1ab57a66f7d0d99d42d322e3b594162b42af077874f9ed35ffa88c8'
+  #be559b54eb4772a703ad35239d1cb59d32f7cf8a739966742622d57df88b896e'
   action [ :configure, :install_with_make ]
 end
